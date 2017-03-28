18fec935e3b08f30947d9a942b9f0a12d1a5b8a8
ArgusMagnus <argusmagnus@outlook.com>
2017-03-28 19:56:02 +0200

    Added copyright/license headers

--- PDFiumSharp.Xwt/RenderingExtensions.cs
+++ PDFiumSharp.Xwt/RenderingExtensions.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿#region Copyright and License
+/*
+This file is part of PDFiumSharp, a wrapper around the PDFium library for the .NET framework.
+Copyright (C) 2017 Tobias Meyer
+License: Microsoft Reciprocal License (MS-RL)
+*/
+#endregion
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
