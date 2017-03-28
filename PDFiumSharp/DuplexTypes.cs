4386f4df05f4b2f23c1cdaf54995cefe21c337f6
ArgusMagnus <argusmagnus@outlook.com>
2017-03-28 16:22:07 +0200

    DuplexTypes enum added

---
+++ PDFiumSharp/DuplexTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDFiumSharp
+{
+    public enum DuplexTypes : int
+    {
+		DuplexUndefined = 0,
+		Simplex,
+		DuplexFlipShortEdge,
+		DuplexFlipLongEdge
+	}
+}
