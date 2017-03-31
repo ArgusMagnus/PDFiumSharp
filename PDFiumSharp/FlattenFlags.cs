9fe8fc228412eafeef543aa49e07e0b728b6d431
ArgusMagnus <argusmagnus@outlook.com>
2017-03-31 13:34:01 +0200

    Added PDFium.FPDFPage_Flatten method

---
+++ PDFiumSharp/FlattenFlags.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDFiumSharp
+{
+	public enum FlattenFlags : int
+	{
+		/// <summary>
+		/// Flatten for normal display.
+		/// </summary>
+		NormalDisplay = 0,
+
+		/// <summary>
+		/// Flatten for print.
+		/// </summary>
+		Print = 1
+	}
+
+	public enum FlattenResults : int
+	{
+		/// <summary>
+		/// Flatten operation failed.
+		/// </summary>
+		Fail = 0,
+
+		/// <summary>
+		/// Flatten operation succeed.
+		/// </summary>
+		Success = 1,
+
+		/// <summary>
+		/// Nothing to be flattened.
+		/// </summary>
+		NothingToDo = 2
+	}
+}
