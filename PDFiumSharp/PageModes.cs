e49ec62c43abd52a690381dd2576ae0fe70d1855
ArgusMagnus <argusmagnus@outlook.com>
2017-03-31 13:17:08 +0200

    ...

---
+++ PDFiumSharp/PageModes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDFiumSharp
+{
+	public enum PageModes : int
+	{
+		/// <summary>
+		/// Unknown page mode.
+		/// </summary>
+		Unknown = -1,
+
+		/// <summary>
+		/// Document outline, and thumbnails hidden.
+		/// </summary>
+		UseNone = 0,
+
+		/// <summary>
+		/// Document outline visible.
+		/// </summary>
+		UseOutlines = 1,
+
+		/// <summary>
+		/// Thumbnail images visible.
+		/// </summary>
+		UseThumbs = 2,
+
+		/// <summary>
+		/// Full-screen mode, no menu bar, window controls, or other decorations visible.
+		/// </summary>
+		Fullscreen = 3,
+
+		/// <summary>
+		/// Optional content group panel visible.
+		/// </summary>
+		UseOC = 4,
+
+		/// <summary>
+		/// Attachments panel visible.
+		/// </summary>
+		UseAttachments = 5
+	}
+}
