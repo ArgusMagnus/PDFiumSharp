d3b61f36fc02e613d3ae8f0c153d41cc5fa861d2
philipspigelmire <philip@weatherite.com>
2018-01-18 17:02:29 -0800

    Fix for out of range exception on page insert. It was possible to call Insert/Add without the _pages List being initialized, some methods checked this, others didn't. I moved the initialization to the constructor.

--- PDFiumSharp/PdfPageCollection.cs
+++ PDFiumSharp/PdfPageCollection.cs
@@ -22,7 +22,11 @@ namespace PDFiumSharp
 		{
 			_doc = doc;
 			_pages = new List<PdfPage>(PDFium.FPDF_GetPageCount(doc.Handle));
-		}
+
+            //Initialize _pages with null entries
+            for(int i=this.Count;i>0;i--)
+                _pages.Add(null);
+        }
 
 		/// <summary>
 		/// Gets the number of pages in the <see cref="PdfDocument"/>.
@@ -38,11 +42,7 @@ namespace PDFiumSharp
 			{
 				if (index >= _pages.Count)
 				{
-					int count = this.Count;
-					if (index >= count)
-						throw new ArgumentOutOfRangeException(nameof(index));
-					while (_pages.Count < count)
-						_pages.Add(null);
+					throw new ArgumentOutOfRangeException(nameof(index));
 				}
 
 				if (_pages[index] == null || _pages[index].IsDisposed)
@@ -60,13 +60,13 @@ namespace PDFiumSharp
 
 		IEnumerator<PdfPage> IEnumerable<PdfPage>.GetEnumerator()
 		{
-			for (int i = 0; i < Count; i++)
+			for (int i = 0; i < _pages.Count; i++)
 				yield return this[i];
 		}
 
 		IEnumerator IEnumerable.GetEnumerator()
 		{
-			for (int i = 0; i < Count; i++)
+			for (int i = 0; i < _pages.Count; i++)
 				yield return this[i];
 		}
 
@@ -76,16 +76,24 @@ namespace PDFiumSharp
 		/// <seealso cref="PDFium.FPDF_ImportPages(Types.FPDF_DOCUMENT, Types.FPDF_DOCUMENT, int, int[])"/>
 		public bool Insert(int index, PdfDocument sourceDocument, params int[] srcPageIndices)
 		{
-			if (index < _pages.Count)
-				_pages.Insert(index, null);
-			var result = PDFium.FPDF_ImportPages(_doc.Handle, sourceDocument.Handle, index, srcPageIndices);
-			_pages.InsertRange(index, Enumerable.Repeat<PdfPage>(null, srcPageIndices.Length));
-			for (int i = index; i < _pages.Count; i++)
-			{
-				if (_pages[i] != null)
-					_pages[i].Index = i;
-			}
-			return result;
+            bool result = false;
+            if (index <= _pages.Count)
+            {
+                result = PDFium.FPDF_ImportPages(_doc.Handle, sourceDocument.Handle, index, srcPageIndices);
+                if (result)
+                {
+                    _pages.InsertRange(index, Enumerable.Repeat<PdfPage>(null, srcPageIndices.Length));
+                    for (int i = index; i < _pages.Count; i++)
+                    {
+                        if (_pages[i] != null)
+                            _pages[i].Index = i;
+                    }
+                }
+            }
+            else
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            return result;
 		}
 
 		/// <summary>
@@ -102,20 +110,21 @@ namespace PDFiumSharp
 		/// </summary>
 		public PdfPage Insert(int index, double width, double height)
 		{
-			if (index < _pages.Count)
+            PdfPage page=null;
+            if (index <= _pages.Count)
 			{
-				_pages.Insert(index, null);
+                page = PdfPage.New(_doc, index, width, height);
+                _pages.Insert(index, page);
 				for (int i = index; i < _pages.Count; i++)
 				{
 					if (_pages[i] != null)
 						_pages[i].Index = i;
 				}
 			}
-			var page = PdfPage.New(_doc, index, width, height);
-			while (_pages.Count <= index)
-				_pages.Add(null);
-			_pages[index] = page;
-			return page;
+            else
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            return page;
 		}
 
 		/// <summary>
